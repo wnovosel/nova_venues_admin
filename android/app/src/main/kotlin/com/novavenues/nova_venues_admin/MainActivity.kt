ad6a83ca05887d1030f938991d93a4a17b5c1025
wnovosel <walt@novocellars.com>
2026-06-30 23:43:59 -0400

    ci: add Xcode Cloud post-clone script + app display name

---
+++ android/app/src/main/kotlin/com/novavenues/nova_venues_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.novavenues.nova_venues_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+rm -rf $HOME/Library/Developer/Xcode/DerivedData
+rm -rf /Volumes/workspace/DerivedData
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+cd $CI_PRIMARY_REPOSITORY_PATH
+flutter pub get
+flutter precache --ios
+
+cd ios
+rm -rf Pods Podfile.lock .symlinks
+pod install --repo-update
