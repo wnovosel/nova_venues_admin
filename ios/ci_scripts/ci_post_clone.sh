f470dcef9ab2b3049bd6412bcfa95b89b0355196
wnovosel <wnovosel@gmail.com>
2026-07-20 23:34:18 -0400

    Fix Xcode Cloud Flutter setup before generating branding assets

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 set -e
 
+rm -rf "$HOME/Library/Developer/Xcode/DerivedData"
+rm -rf /Volumes/workspace/DerivedData
+
+if [ ! -d "$HOME/flutter" ]; then
+  git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
+fi
+export PATH="$PATH:$HOME/flutter/bin"
+
 cd "$CI_PRIMARY_REPOSITORY_PATH"
+flutter config --no-analytics
 flutter pub get
+flutter precache --ios
+
 dart run flutter_launcher_icons
 dart run flutter_native_splash:create
+
+cd ios
+rm -rf Pods Podfile.lock .symlinks
+pod install
